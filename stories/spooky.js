173423337bdbf1246c57fbb34e007430eff8e849
Marc Rudkowski <marc.rudkowski@gmail.com>
2018-10-01 20:13:44 -0400

    Adds Victory to repo

--- stories/spooky.js
+++ stories/spooky.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryBar } from 'victory';
 import { storiesOf } from '@storybook/react';
 
 storiesOf('Initial Test', module)
   .add('Spooky', () => (
-    <p> Spooky Test </p>
+    <VictoryBar />
   ));
